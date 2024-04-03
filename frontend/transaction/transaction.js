172b0cd468f1a837c960e400f3f01ebd0bb10921
Hailey1005 <nguyenngochang510@gmail.com>
2024-04-02 22:13:31 -0500

    added transactions and pop ups

--- frontend/transaction/transaction.js
+++ frontend/transaction/transaction.js
@@ -1,5 +1,430 @@
+'use strict'
 
+let data = [
+    {
+        "id": 1,
+        "date": "03/17/24",
+        "service": "Etsy",
+        "cost": 25.69,
+        "items": [
+            {
+                "desc": "Nautro artwork",
+                "qty": 1,
+                "cost": 23.42
+            },
+            {
+                "desc": "Nautro pins",
+                "qty": 3,
+                "cost": 23.42
+            }
+        ]
+    },
+    {
+        "id": 2,
+        "date": "04/20/24",
+        "service": "Etsy",
+        "cost": 36.47,
+        "items": [
+            {
+                "desc": "Cow print",
+                "qty": 1,
+                "cost": 23.42
+            },
+            {
+                "desc": "Pictures of Alfred",
+                "qty": 9,
+                "cost": 23.42
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "date": "04/20/24",
+        "service": "Etsy",
+        "cost": 4.35,
+        "items": [
+            {
+                "desc": "Flower bookmark",
+                "qty": 8,
+                "cost": 24.42
+            },
+            {
+                "desc": "dino keychain",
+                "qty": 2,
+                "cost": 11.22
+            }
+          
+        ]
+    },
+    {
+        "id": 4,
+        "date": "04/20/24",
+        "service": "Etsy",
+        "cost": 35.69,
+        "items": [
+            {
+                "desc": "Heart bookmark",
+                "qty": 8,
+                "cost": 24.42
+            },
+            {
+                "desc": "Chicken keychain",
+                "qty": 2,
+                "cost": 11.22
+            }
+        ]
+    },
+    {
+        "id": 5,
+        "date": "04/20/24",
+        "service": "Etsy",
+        "cost": 32.69,
+        "items": [
+            {
+                "desc": "Pumpkin bookmark",
+                "qty": 3,
+                "cost": 20.43
+            },
+            {
+                "desc": "worm bookmark",
+                "qty": 2,
+                "cost": 12.27
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "date": "04/20/24",
+        "service": "Etsy",
+        "cost": 42.69,
+        "items": [
+            {
+            "desc": "Flower bookmark",
+            "qty": 8,
+            "cost": 24.42
+             },
+             {
+            "desc": "dino keychain",
+            "qty": 2,
+            "cost": 11.22
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "date": "04/20/24",
+        "service": "Etsy",
+        "cost": 12.39,
+        "items": [
+            {
+                "desc": "dino bookmark",
+                "qty": 3,
+                "cost": 20.43
+            },
+            {
+                "desc": "worm bookmart",
+                "qty": 2,
+                "cost": 12.27
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "date": "04/20/24",
+        "service": "Etsy",
+        "cost": 8.53,
+        "items": [
+            {
+                "desc": "pumpkin bookmark",
+                "qty": 3,
+                "cost": 20.43
+            },
+            {
+                "desc": "worm bookmart",
+                "qty": 2,
+                "cost": 12.27
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "date": "04/20/24",
+        "service": "Etsy",
+        "cost": 23.65,
+        "items": [
+            {
+                "desc": "heart bookmark",
+                "qty": 3,
+                "cost": 20.43
+            },
+            {
+                "desc": "worm bookmart",
+                "qty": 2,
+                "cost": 12.27
+            }
+        ]
+    },
+    {
+        "id": 12.39,
+        "date": "04/20/24",
+        "service": "Etsy",
+        "cost": 4.69,
+        "items": [
+            {
+                "desc": "heart bookmark",
+                "qty": 3,
+                "cost": 20.43
+            },
+            {
+                "desc": "worm bookmart",
+                "qty": 2,
+                "cost": 12.27
+            }
+        ]
+    },
+    {
+        "id": 8.47,
+        "date": "04/20/24",
+        "service": "Etsy",
+        "cost": 42.69,
+        "items": [
+            {
+                "desc": "heart bookmark",
+                "qty": 3,
+                "cost": 20.43
+            },
+            {
+                "desc": "worm bookmart",
+                "qty": 2,
+                "cost": 12.27
+            }
+        ]
+    },
+    {
+        "id": 3,
+        "date": "04/20/24",
+        "service": "PEtsy",
+        "cost": 45.89,
+        "items": [
+            {
+                "desc": "heart bookmark",
+                "qty": 3,
+                "cost": 20.43
+            },
+            {
+                "desc": "worm bookmart",
+                "qty": 2,
+                "cost": 12.27
+            }
+        ]
+    }
+    
+];
 
+let withdrawData = [
+    {
+        "id": 9,
+        "date": "03/17/24",
+        "service": "JOANN",
+        "cost":74.69,
+        "items": [
+            {
+                "desc": "Blanket Yarn - Pink",
+                "qty": 10,
+                "cost": 12.42
+            },
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 14.69
+            }
+        ]
+    },
+    
+    {
+        "id": 7,
+        "date": "03/17/24",
+        "service": "Michaels Store",
+        "cost":54.69,
+        "items": [
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 14.69
+            },
+            {
+                "desc": "Blanket Yarn - Pink",
+                "qty": 1,
+                "cost": 14.69
+            },
+            {
+                "desc": "Blanket Yarn - Stripe",
+                "qty": 1,
+                "cost": 14.69
+            }
+            
+            
+        ]
+    },
+    {
+        "id": 9,
+        "date": "03/17/24",
+        "service": "JOANN",
+        "cost":33.45,
+        "items": [
+            {
+                "desc": "Blanket Yarn - Pink",
+                "qty": 1,
+                "cost": 12.42
+            },
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 14.69
+            }
+        ]
+    },
+    {
+        "id": 7,
+        "date": "03/17/24",
+        "service": "Michaels Store",
+        "cost": 14.69,
+        "items": [
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 14.69
+            }
+            
+        ]
+    },
+    {
+        "id": 7,
+        "date": "03/17/24",
+        "service": "Michaels Store",
+        "cost": 14.69,
+        "items": [
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 14.69
+            }
+            
+        ]
+    },
+    {
+        "id": 7,
+        "date": "03/17/24",
+        "service": "Premier Yarn",
+        "cost": 19.35,
+        "items": [
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 14.69
+            },
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 14.69
+            }
+            
+        ]
+    },
+    {
+        "id": 7,
+        "date": "03/17/24",
+        "service": "Premier Yarn",
+        "cost": 44.69,
+        "items": [
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 14.69
+            },
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 14.69
+            },
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 14.69
+            }
+            
+        ]
+    },
+    {
+        "id": 7,
+        "date": "03/17/24",
+        "service": "Premier Yarn",
+        "cost": 45.67,
+        "items": [
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 1.69
+            }
+            
+        ]
+    },
+    {
+        "id": 9,
+        "date": "03/17/24",
+        "service": "JOANN",
+        "cost":13.45,
+        "items": [
+            {
+                "desc": "Blanket Yarn - Pink",
+                "qty": 1,
+                "cost": 12.42
+            },
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 14.69
+            }
+        ]
+    },
+    {
+        "id": 9,
+        "date": "03/17/24",
+        "service": "JOANN",
+        "cost":13.45,
+        "items": [
+            {
+                "desc": "Blanket Yarn - Pink",
+                "qty": 1,
+                "cost": 12.42
+            },
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 19.45
+            }
+        ]
+    },
+    {
+        "id": 9,
+        "date": "03/17/24",
+        "service": "JOANN",
+        "cost":13.45,
+        "items": [
+            {
+                "desc": "Blanket Yarn - Pink",
+                "qty": 1,
+                "cost": 12.42
+            },
+            {
+                "desc": "Blanket Yarn - Blue",
+                "qty": 1,
+                "cost": 14.69
+            }
+        ]
+    }
+
+];
 
 /// create our click event listener
 function openPopup(e) {
@@ -7,9 +432,26 @@ function openPopup(e) {
     if ( !e ){ e = window.Event };
     /// fallback support for IE .srcElement
     // alert( 'Clicked! ' + ( e.target ? e.target : e.srcElement ) );
-    console.log(this)
-    console.log(e)
+    popupDeposit(getDataById(data, this.id))
     depositPopup.classList.add("show");
+}
+
+function openWithdrawPopup(e) {
+    /// fallback support for IE events
+    if ( !e ){ e = window.Event };
+    /// fallback support for IE .srcElement
+    // alert( 'Clicked! ' + ( e.target ? e.target : e.srcElement ) );
+    popupDeposit(getDataById(withdrawData, this.id))
+    depositPopup.classList.add("show");
+}
+
+function getDataById(dataItem, id) {
+    var foundData = dataItem.filter(
+        // Note: id is itemId<id> I.e., itemId1
+        function(dataItem){ return "itemId" + dataItem.id == id }
+    );
+    console.log(foundData, id, dataItem)
+    return foundData[0]
 }
 
 function closePopup() {
@@ -34,11 +476,8 @@ function popupDeposit(dataItem) {
         liItem.setAttribute("class", "info")
         // ulItem.appendChild = liItem;
         liItem.innerHTML = `${i+1}.<span class="card item fill">${item.desc}</span> <span class="card item">${item.qty}</span>`;
-        console.log(liItem)
         ulItem.appendChild(liItem);
-    })
-    
-    console.log(ulItem)
+    });
 
     let bodyText = `
         <h3>Sale Information</h3>
@@ -54,126 +493,20 @@ function popupDeposit(dataItem) {
         </div>
         <button id="closeDepositPopup">OK</button>
     `
-
-
     depositBody.innerHTML = bodyText;
     closeDepositPopup.addEventListener("click", closePopup);
 
-   
 }
 
 
 (function() {
 
-    let data = [
-        {
-            "id": 1,
-            "date": "03/17/24",
-            "service": "Etsy",
-            "cost": 25.69,
-            "items": [
-                {
-                    "desc": "Nautro artwork",
-                    "qty": 1,
-                    "cost": 23.42
-                },
-                {
-                    "desc": "Nautro pins",
-                    "qty": 3,
-                    "cost": 23.42
-                }
-            ]
-        },
-        {
-            "id": 2,
-            "date": "04/20/24",
-            "service": "Micheals Stores",
-            "cost": 942.69,
-            "items": [
-                {
-                    "desc": "Cow print",
-                    "qty": 1,
-                    "cost": 23.42
-                },
-                {
-                    "desc": "Pictures of Alfred",
-                    "qty": 9,
-                    "cost": 23.42
-                }
-            ]
-        },
-        {
-            "id": 3,
-            "date": "04/20/24",
-            "service": "Facebook",
-            "cost": 324.35,
-            "items": [
-                {
-                    "desc": "printers print",
-                    "qty": 1,
-                    "cost": 23.42
-                },
-                {
-                    "desc": "ink",
-                    "qty": 9,
-                    "cost": 2.42
-                },
-                {
-                    "desc": "paper",
-                    "qty": 9,
-                    "cost": 33.42
-                },
-                {
-                    "desc": "color paper",
-                    "qty": 3,
-                    "cost": 50.42
-                }
-            ]
-        },
-        {
-            "id": 4,
-            "date": "04/20/24",
-            "service": "Ebay",
-            "cost": 3415.69,
-            "items": [
-                {
-                    "desc": "Gameboy",
-                    "qty": 1,
-                    "cost": 244.42
-                },
-                {
-                    "desc": "Switch",
-                    "qty": 2,
-                    "cost": 400.00
-                }
-            ]
-        },
-        {
-            "id": 3,
-            "date": "04/20/24",
-            "service": "Paypal",
-            "cost": 4532.69,
-            "items": [
-                {
-                    "desc": "Bitcoin",
-                    "qty": 1,
-                    "cost": 909832.42
-                },
-                {
-                    "desc": "Ethereum",
-                    "qty": 20,
-                    "cost": 223423.42
-                }
-            ]
-        }
-        
-    ]
     let depositList = document.getElementById("depositList");
     
     data.forEach((item) => {
-
         let liItem = document.createElement("li");
         liItem.setAttribute("class", "info")
+        liItem.setAttribute("id", `itemId${item.id}`)
         let dateText = `<span class="item">${item.date}</span>`
         let serviceText = `<span class="item fill">${item.service}</span>`
         let costText = `<span class="item">${item.cost}</span>`
@@ -182,26 +515,28 @@ function popupDeposit(dataItem) {
         depositList.appendChild(liItem);
     });
 
-    data.forEach((item) => {
-
+    withdrawData.forEach((item) => {
         let liItem = document.createElement("li");
         liItem.setAttribute("class", "info")
+        liItem.setAttribute("id", `itemId${item.id}`)
         let dateText = `<span class="item">${item.date}</span>`
         let serviceText = `<span class="item fill">${item.service}</span>`
         let costText = `<span class="item">${item.cost}</span>`
         liItem.innerHTML = dateText + serviceText + costText;
-        liItem.onclick = openPopup;
+        liItem.onclick = openWithdrawPopup;
         withdrawList.appendChild(liItem);
     });
     
     
     window.addEventListener("click",closeWindow);
 
-    var elems = document.getElementsByTagName('li');
-    Array.from(elems).forEach((v,i) => v.addEventListener('click', function() {
-        popupDeposit(data[i])
-    }));
-    
+    // var elems = document.getElementsByTagName('li');
+    // var elems = document.getElementById('itemId')
+    // Array.from(elems).forEach((v,i) => v.addEventListener('click', function() {
+    //     console.log("dog", elems)
+    //     console.log(data[i])
+    //     popupDeposit(data[i])
+    // }));
     
     
 })();
