2412b716df80b161e4fd3deea74393bfcf2926e2
Galen Schick <gschick3@gmail.com>
2024-04-13 18:12:32 -0500

    add EntityNotFoundException for string id

--- backend/src/main/java/com/recinven/recinvenbackend/exceptions/exception/EntityNotFoundException.java
+++ backend/src/main/java/com/recinven/recinvenbackend/exceptions/exception/EntityNotFoundException.java
@@ -4,4 +4,8 @@ public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(Class<?> c, Long id) {
         super(String.format("Could not find %s id: %d", c.getSimpleName(), id));
     }
+
+    public EntityNotFoundException(Class<?> c, String id) {
+        super(String.format("Could not find %s id: %s", c.getSimpleName(), id));
+    }
 }
