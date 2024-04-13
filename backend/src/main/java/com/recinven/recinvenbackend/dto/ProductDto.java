18d392e512db9759367aa0d0ed53ce57911ca1c3
Galen Schick <gschick3@gmail.com>
2024-04-13 13:44:42 -0500

    fix Product field visibility

--- backend/src/main/java/com/recinven/recinvenbackend/dto/ProductDto.java
+++ backend/src/main/java/com/recinven/recinvenbackend/dto/ProductDto.java
@@ -3,6 +3,4 @@ package com.recinven.recinvenbackend.dto;
 public class ProductDto {
     public String description;
     public float currentPrice;
-    public int totalSold;
-    public float totalEarned;
 }
--- backend/src/main/java/com/recinven/recinvenbackend/entity/Product.java
+++ backend/src/main/java/com/recinven/recinvenbackend/entity/Product.java
@@ -1,6 +1,7 @@
 package com.recinven.recinvenbackend.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -16,16 +17,23 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id")
     private Long productId;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     @JsonIgnore
     private User user;
+
     @Column(name = "description", nullable = false)
     private String description;
+
     @Column(name = "current_price", nullable = false)
     private float currentPrice;
+
     @Column(name = "total_sold")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private int totalSold;
+
     @Column(name = "total_earned")
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private float totalEarned;
 }
