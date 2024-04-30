8689c5fe93cd43371e43c9966e395bc0420e1339
Galen Schick <gschick3@gmail.com>
2024-04-30 12:33:17 -0500

    add CORS support on backend

--- backend/src/main/java/com/recinven/recinvenbackend/security/WebSecurityConfig.java
+++ backend/src/main/java/com/recinven/recinvenbackend/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.recinven.recinvenbackend.security;
 
+import com.recinven.recinvenbackend.security.jwt.AuthEntryPointJwt;
 import com.recinven.recinvenbackend.security.jwt.AuthTokenFilter;
 import com.recinven.recinvenbackend.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,8 +17,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.recinven.recinvenbackend.security.jwt.AuthEntryPointJwt;
 
 @Configuration
 @EnableMethodSecurity
